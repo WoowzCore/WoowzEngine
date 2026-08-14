b4dbb16478ac9c321bd4bfdecbd106228f195f0c
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-08-14 19:33:21 +0300

    Начало создания редактора

--- WoowzEngine/test2.cs
+++
@@ -1,7 +0,0 @@
-﻿using WoowzLib.Core;
-
-namespace WoowzEngine;
-
-public class test2{
-    public const string TEST2222 = test.TEST;
-}
---
+++ WoowzEngineEditor/Program.cs
@@ -0,0 +1,12 @@
+﻿using WLO.Math;
+
+WLO.Window.GLFW Window = new WLO.Window.GLFW(new Vector2I(800, 600), "WOOWZ ENGINE EDITOR");
+
+while(!Window.IsClosed){
+    Window.PollEvents();
+    
+    
+}
+
+Window.Close();
+WL.GLFW.Stop();
