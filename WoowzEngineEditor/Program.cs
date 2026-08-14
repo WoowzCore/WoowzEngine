7efc8e9f9c0e3c1d696f42e7a779ff9536a278b4
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-08-14 20:45:25 +0300

    Добавлен вулкан

--- WoowzEngineEditor/Program.cs
+++ WoowzEngineEditor/Program.cs
@@ -1,12 +1,18 @@
 ﻿using WLO.Math;
+using WLO.Render.Hardware;
 
 WLO.Window.GLFW Window = new WLO.Window.GLFW(new Vector2I(800, 600), "WOOWZ ENGINE EDITOR");
+
+Vulkan Render = new WLO.Render.Hardware.Vulkan();
+Render.Start();
 
 while(!Window.IsClosed){
     Window.PollEvents();
     
     
 }
+
+Render.Stop();
 
 Window.Close();
 WL.GLFW.Stop();
