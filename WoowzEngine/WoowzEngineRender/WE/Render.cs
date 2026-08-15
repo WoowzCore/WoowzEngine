c7ede4b881079884e13d922ed37f4b1f3baabc5c
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-08-15 20:22:35 +0300

    МИКРО макро

--- WoowzEngine/WoowzEngineRender/WE/Render.cs
+++ WoowzEngine/WoowzEngineRender/WE/Render.cs
@@ -1,10 +1,11 @@
 ﻿using WLI;
 using WLO;
+using WLO.Render.Hardware;
 
 namespace WE;
 
 public static class Render{
-    private const string Prefix = "WERender";
+    private const string Prefix = "WER";
 
     public static bool IsStarted{ get; private set; }
 
@@ -19,7 +20,7 @@ public static class Render{
     }
     
     public struct StartProperties{
-        public Logger? UseThisLogger;
+        public Logger? UseThisLogger; // todo, remake, look vulkan reference
     }
     
     public static void Start(StartProperties? Properties = null){
@@ -32,9 +33,15 @@ public static class Render{
             
             Log(LogType_Initialization, $"Запуск WoowzEngineRender...\nПараметры: {Properties}");
             
+            // Запуск Vulkan
+            Vulkan = new Vulkan(new Vulkan.StartParameters{
+                UseThisLogger = CurrentLogger, // FIX THAT LATER, TODO, я не должен тут указывать логгер, почему-то он не видит оригинал
+                DebugLogger = true
+            }, true);
+            
             IsStarted = true;
         }catch(Exception e){
-            throw new ExceptionWL("Произошла ошибка при запуске WoowzEngineRender!\nWE.Render.Start()", e);
+            throw new ExceptionWL("Произошла ошибка при запуске WoowzEngineRender!", e);
         }
     }
 
@@ -42,11 +49,17 @@ public static class Render{
         try{
             if(!IsStarted){ throw new ExceptionWL("Рендер не был даже запущен!"); }
             
-            Log(LogType_Initialization, "Остановка WoowzEngineRender!");
+            Log(LogType_Initialization, "Остановка WoowzEngineRender...");
+
+            if(Vulkan != null! && Vulkan.IsStarted){
+                Vulkan.Stop();
+            }
             
             IsStarted = false;
         }catch(Exception e){
-            throw new ExceptionWL("Произошла ошибка при остановке WoowzEngineRender!\nWE.Render.Stop()", e);
+            throw new ExceptionWL("Произошла ошибка при остановке WoowzEngineRender!", e);
         }
     }
+
+    private static WLO.Render.Hardware.Vulkan Vulkan;
 }
